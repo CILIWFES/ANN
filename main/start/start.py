0232fae97b711816272b19d8ef321ee1c95bf556
CILIWFES <CILIWFES@163.com>
2019-02-19 23:49:49 +0800

    gpu引擎安装完毕

--- main/start/start.py
+++ main/start/start.py
@@ -1,3 +1,8 @@
 from main.configuration import *
 
-print(GLOCF.getConfig(GLOCT.ORM_CONFIG_SECTION, GLOCT.SAVE_FOLDER_CONFIG))
+import mxnet as mx
+print("xx")
+a = mx.nd.ones((2, 3), mx.gpu())
+print(a)
+print((mx.nd.ones((2, 2), mx.cpu()) * 100).asnumpy())
+print((mx.nd.ones((2, 2), mx.gpu()) * 100).asnumpy())
